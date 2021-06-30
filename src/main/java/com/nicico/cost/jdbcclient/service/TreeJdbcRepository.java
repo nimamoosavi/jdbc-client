a46d7a9bc2aae0557d283c81d8cf8aca349c816a
hossein <mahdevarhossein@gmail.com>
2021-06-30 09:45:34 +0430

    tree moved

---
+++ src/main/java/com/nicico/cost/jdbcclient/service/TreeJdbcRepository.java
@@ -0,0 +1,22 @@
+package com.nicico.cost.jdbcclient.service;
+
+import com.nicico.cost.jdbcclient.repository.JdbcRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface TreeJdbcRepository<T, I extends Serializable> extends JdbcRepository<T, I> {
+
+    List<T> findAllByParentId(I id);
+
+    Page<T> findAllByParentId(I id, Pageable pageable);
+
+    List<T> findAllByParentIdIsNull();
+
+    List<T> findAllByParentIdIsNull(Sort orders);
+
+    Page<T> findAllByParentIdIsNull(Pageable pageable);
+}
---
+++ src/main/java/com/nicico/cost/jdbcclient/service/impl/TreeJdbcServiceImpl.java
@@ -0,0 +1,68 @@
+package com.nicico.cost.jdbcclient.service.impl;
+
+import com.nicico.cost.framework.domain.dto.PageDTO;
+import com.nicico.cost.framework.packages.crud.view.Sort;
+import com.nicico.cost.crud.repository.TreeRepository;
+import com.nicico.cost.jdbcclient.service.TreeJdbcRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.io.Serializable;
+import java.util.List;
+
+public abstract class TreeJdbcServiceImpl<T, I extends Serializable> extends JdbcServiceImpl<T, I> implements TreeRepository<T, I> {
+
+
+    @Autowired
+    private TreeJdbcRepository<T, I> treeJdbcRepository;
+
+    @Override
+    public List<T> findAll(I pid) {
+        return treeJdbcRepository.findAllByParentId(pid);
+    }
+
+    @Override
+    public List<T> findAllParent() {
+        return treeJdbcRepository.findAllByParentIdIsNull();
+    }
+
+    @Override
+    public List<T> findAllParent(List<Sort> orders) {
+        org.springframework.data.domain.Sort sort = sort(orders);
+        return treeJdbcRepository.findAllByParentIdIsNull(sort);
+    }
+
+    @Override
+    public PageDTO<List<T>> findAllParent(int page, int pageSize, List<Sort> orders) {
+        Pageable pagination = pagination(page, pageSize, orders);
+        Page<T> allByParentIdIsNull = treeJdbcRepository.findAllByParentIdIsNull(pagination);
+        return convertToPageDTO(allByParentIdIsNull);
+    }
+
+    @Override
+    public PageDTO<List<T>> findAllParent(int page, int pageSize) {
+        Pageable pagination = pagination(page, pageSize);
+        Page<T> allByParentIdIsNull = treeJdbcRepository.findAllByParentIdIsNull(pagination);
+        return convertToPageDTO(allByParentIdIsNull);
+    }
+
+    @Override
+    public PageDTO<List<T>> findAll(int page, int pageSize, I pid) {
+        Pageable pageable = pagination(page, pageSize);
+        Page<T> allByParentId = treeJdbcRepository.findAllByParentId(pid, pageable);
+        return convertToPageDTO(allByParentId);
+    }
+
+    @Override
+    public PageDTO<List<T>> findAll(int page, int pageSize, List<Sort> orders, I pid) {
+        Pageable pageable = pagination(page, pageSize, orders);
+        Page<T> allByParentId = treeJdbcRepository.findAllByParentId(pid, pageable);
+        return convertToPageDTO(allByParentId);
+    }
+
+
+    private PageDTO<List<T>> convertToPageDTO(Page<T> page){
+        return PageDTO.<List<T>>builder().object(page.toList()).pageSize(page.getSize()).totalElement(page.getTotalElements()).totalPages(page.getTotalPages()).build();
+    }
+}
