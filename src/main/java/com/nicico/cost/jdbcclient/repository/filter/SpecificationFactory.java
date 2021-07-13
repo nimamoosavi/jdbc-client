0f83dd8f4fb9d850b65ddafd9625f4d7a68fb455
nima <nmoosavi@live.com>
2021-07-13 14:29:31 +0430

    add cost and account to integration

--- src/main/java/com/nicico/cost/jdbcclient/repository/filter/SpecificationFactory.java
+++ src/main/java/com/nicico/cost/jdbcclient/repository/filter/SpecificationFactory.java
@@ -4,6 +4,7 @@ import com.nicico.cost.framework.packages.crud.view.Criteria;
 import com.nicico.cost.framework.packages.crud.view.Operator;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
+
 import javax.annotation.PostConstruct;
 import javax.persistence.criteria.Join;
 import java.util.HashMap;
@@ -13,6 +14,7 @@ import java.util.function.Function;
 @Component
 public class SpecificationFactory<T> {
     private Map<Integer, Function<Criteria, Specification<T>>> specs;
+
     @PostConstruct
     private void init() {
         specs = new HashMap<>();
@@ -40,10 +42,10 @@ public class SpecificationFactory<T> {
 
     private Specification<T> getEqualsSpecification(Criteria criteria) {
         return (root, query, builder) -> {
-            if(criteria.getFieldName().contains(".")){
+            if (criteria.getFieldName().contains(".")) {
                 String[] arrStr = criteria.getFieldName().split("\\.");
-                Join join = root.join(arrStr[0]);
-                return builder.equal(join.get(arrStr[1]),criteria.getValue());
+                Join<Object, Object> join = root.join(arrStr[0]);
+                return builder.equal(join.get(arrStr[1]), criteria.getValue());
             }
             return builder.equal(root.get(criteria.getFieldName()), criteria.getValue());
         };
@@ -51,66 +53,84 @@ public class SpecificationFactory<T> {
 
     private Specification<T> getNotEqualsSpecification(Criteria criteria) {
         return (root, query, builder) -> {
-            if(criteria.getFieldName().contains(".")){
+            if (criteria.getFieldName().contains(".")) {
                 String[] arrStr = criteria.getFieldName().split("\\.");
-                Join join = root.join(arrStr[0]);
-                return builder.notEqual(join.get(arrStr[1]),criteria.getValue());
+                Join<Object, Object> join = root.join(arrStr[0]);
+                return builder.notEqual(join.get(arrStr[1]), criteria.getValue());
             }
-           return builder.notEqual(root.get(criteria.getFieldName()), criteria.getValue());
+            return builder.notEqual(root.get(criteria.getFieldName()), criteria.getValue());
         };
     }
+
+    @SuppressWarnings("unchecked")
     private Specification<T> getLessThanSpecification(Criteria criteria) {
         return (root, query, builder) ->
                 builder.lessThan(root.get(criteria.getFieldName()), (Comparable<Object>) criteria.getValue());
     }
+
+    @SuppressWarnings("unchecked")
     private Specification<T> getGreaterThanSpecification(Criteria criteria) {
         return (root, query, builder) ->
                 builder.greaterThan(root.get(criteria.getFieldName()), (Comparable<Object>) criteria.getValue());
     }
+
+    @SuppressWarnings("unchecked")
     private Specification<T> getGreaterOrEqualsSpecification(Criteria criteria) {
         return (root, query, builder) ->
                 builder.greaterThanOrEqualTo(root.get(criteria.getFieldName()), (Comparable<Object>) criteria.getValue());
     }
+
+    @SuppressWarnings("unchecked")
     private Specification<T> getLessOrEqualsSpecification(Criteria criteria) {
         return (root, query, builder) ->
                 builder.lessThanOrEqualTo(root.get(criteria.getFieldName()), (Comparable<Object>) criteria.getValue());
     }
+
     private Specification<T> getContainsSpecification(Criteria criteria) {
         return (root, query, builder) ->
                 root.get(criteria.getFieldName()).in(criteria.getValue());
     }
+
     private Specification<T> getStartWithSpecification(Criteria criteria) {
         return (root, query, builder) ->
-                builder.like(root.get(criteria.getFieldName()),  criteria.getValue()+"%");
+                builder.like(root.get(criteria.getFieldName()), criteria.getValue() + "%");
     }
+
     private Specification<T> getEndWithSpecification(Criteria criteria) {
         return (root, query, builder) ->
-                builder.like(root.get(criteria.getFieldName()),  "%"+criteria.getValue());
+                builder.like(root.get(criteria.getFieldName()), "%" + criteria.getValue());
     }
+
     private Specification<T> getNotContainsSpecification(Criteria criteria) {
         return (root, query, builder) ->
-                builder.notLike(root.get(criteria.getFieldName()), "%"+criteria.getValue()+"%");
+                builder.notLike(root.get(criteria.getFieldName()), "%" + criteria.getValue() + "%");
     }
+
     private Specification<T> getNotStartWithSpecification(Criteria criteria) {
         return (root, query, builder) ->
-                builder.notLike(root.get(criteria.getFieldName()), "%"+ criteria.getValue());
+                builder.notLike(root.get(criteria.getFieldName()), "%" + criteria.getValue());
     }
+
     private Specification<T> getNotEndWithSpecification(Criteria criteria) {
         return (root, query, builder) ->
-                builder.notLike(root.get(criteria.getFieldName()), criteria.getValue()+"%");
+                builder.notLike(root.get(criteria.getFieldName()), criteria.getValue() + "%");
     }
+
     private Specification<T> getBlankSpecification(Criteria criteria) {
         return (root, query, builder) ->
                 builder.equal(root.get(criteria.getFieldName()), "");
     }
+
     private Specification<T> getNotBlankSpecification(Criteria criteria) {
         return (root, query, builder) ->
                 builder.notEqual(root.get(criteria.getFieldName()), "");
     }
+
     private Specification<T> getNullSpecification(Criteria criteria) {
         return (root, query, builder) ->
                 builder.isNull(root.get(criteria.getFieldName()));
     }
+
     private Specification<T> getNotNullSpecification(Criteria criteria) {
         return (root, query, builder) ->
                 builder.isNotNull(root.get(criteria.getFieldName()));
